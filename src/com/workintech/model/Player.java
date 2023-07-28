2741a8713cff1f827623aabf9ebe685da8480a29
kubrasertkaya <118736856+kubrasertkaya@users.noreply.github.com>
2023-07-28 11:09:33 +0300

    player sınıfına başlandı.

---
+++ src/com/workintech/model/Player.java
@@ -0,0 +1,8 @@
+package com.workintech.model;
+
+public class Player {
+     private String name;
+     private int healthPercentage;
+     private Weapon weapon;
+
+}
---
+++ src/com/workintech/model/Weapon.java
@@ -0,0 +1,24 @@
+package com.workintech.model;
+
+public enum Weapon {
+    SWORD(10,1.8),
+    AXE(15,1.5),
+    MACE(20,1.2);
+
+    private int damage;
+    private double attackSpeed;
+
+    Weapon(int damage,double attackSpeed){
+        this.damage=damage;
+        this.attackSpeed=  attackSpeed;
+
+    }
+
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+}
